94fa385235593e953f9bd920263f8ff289b9a73a
djazaj <kazakd@mail.ru>
2022-12-08 12:51:57 +0300

    Initial Commit

---
+++ trt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trt
+//
+//  Created by Dmitry Kazak on 8.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
